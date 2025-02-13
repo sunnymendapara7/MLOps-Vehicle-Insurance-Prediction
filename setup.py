97d0008832b167432bb480f1c50bda5aa0fe71d7
sunnymendapara7 <sunnymendapara09@gmail.com>
2025-02-13 11:45:46 +0530

    project structure ready,venv set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Sunny Mendapara",
+    author_email="sunnymendapara09@gmail.com",
+    packages=find_packages()
+)
